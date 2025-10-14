2bd8976124c1718351ca53e0b6cb6b8beecf29ae
hitesh-malkiya <malkiyahitesh48@gmail.com>
2025-10-14 07:46:45 +0530

    resposive and all fu redy 6

--- app/[slug]/page.js
+++ app/[slug]/page.js
@@ -3,9 +3,11 @@ import { getProductsSort } from '@/lib/Getproduct';
 import React from 'react'
 import Getproduct from '../components/Getproduct';
 import NotFound from '../not-found';
+import { AddToCartButton } from '../components/AddToCartButton';
+import { Buybtn } from '../components/Buybtn';
 
 
-async function Page({searchParams, params }) {
+async function Page({ searchParams, params }) {
 
 
 
@@ -17,39 +19,38 @@ async function Page({searchParams, params }) {
     session = null
   }
 
- 
 
 
-    const tamp= await searchParams
-    const slugtamp= await params;
-    const { slug } = slugtamp
-    const queryString = new URLSearchParams(tamp).toString();
-    let productsData = [];
 
-    try {
-      const res = await getProductsSort(queryString, slug , 'admin' );
-      productsData = res?.data?.products || [];
-    } catch (err) {
- 
-      productsData = [];
-    }
-    
-if(productsData.length == 0){
-    return(
-<NotFound/>
+  const tamp = await searchParams
+  const slugtamp = await params;
+  const { slug } = slugtamp
+  const queryString = new URLSearchParams(tamp).toString();
+  let productsData = [];
+
+  try {
+    const res = await getProductsSort(queryString, slug, 'admin');
+    productsData = res?.data?.products || [];
+  } catch (err) {
+
+    productsData = [];
+  }
+
+  if (productsData.length == 0) {
+    return (
+      <NotFound />
     )
-}
+  }
 
 
   return (
-    <div>
-         
-          <Getproduct productData={productsData}>
-            <button className=" bg-amber-50 text-black  px-4 py-2 rounded-lg text-sm hover:bg-blue-700 transition duration-300">
-              Add to Cart
-            </button>
-          </Getproduct>
-        </div>
+    <div className='mt-24'>
+
+      <Getproduct productData={productsData}>
+        <AddToCartButton />
+        <Buybtn />
+      </Getproduct>
+    </div>
   )
 }
 
